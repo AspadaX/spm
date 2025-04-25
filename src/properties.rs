96806d531a914c90b009fed9a7b8566226ec3823
Xinyu Bao <34500975+AspadaX@users.noreply.github.com>
2025-04-25 10:24:47 +0800

    Create properties.rs

---
+++ src/properties.rs
@@ -0,0 +1,6 @@
+pub static DEFAULT_SPM_FOLDER: &str = ".spm";
+pub static DEFAULT_SPM_PACKAGES_FOLDER: &str = "packages";
+pub static DEFAULT_PACKAGE_JSON: &str = "package.json";
+pub static DEFAULT_EXECUTABLE_ENTRYPOINT: &str = "main.sh";
+pub static DEFAULT_LIBRARUY_ENTRYPOINT: &str = "lib.sh";
+pub static DEFAULT_TEMPORARY_FOLDER: &str = "tmp";
